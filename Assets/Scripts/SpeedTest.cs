c0eb5465b54be9fd1539d5cdd34860a2c18723d1
lst20082008 <sglst123@gmail.com>
2022-11-06 21:44:56 +0800

    Left Right Hand

--- Assets/Scripts/SpeedTest.cs
+++ Assets/Scripts/SpeedTest.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 public class SpeedTest : MonoBehaviour
 {
-    public GameObject text;
     public float finalSpeed;
     public GameObject hp;
-    public GameObject target;
+    public bool flipHand;
+    public GameObject target_left;
+    public GameObject target_right;
     private bool isDown;
     private bool notCross;
     private Vector2 lastMousePos;
@@ -27,6 +28,17 @@ public class SpeedTest : MonoBehaviour
             isDown = true;
             notCross = true;
             lastMousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            if (Input.mousePosition.x < Screen.width / 2)
+            {
+                flipHand = false;
+                target_left.transform.parent.parent.gameObject.SetActive(true);
+                target_right.transform.parent.parent.gameObject.SetActive(false);
+            }
+            else {
+                flipHand = true;
+                target_left.transform.parent.parent.gameObject.SetActive(false);
+                target_right.transform.parent.parent.gameObject.SetActive(true);
+            }
             Debug.Log("down");
         }
         if (Input.GetMouseButtonUp(0))
@@ -35,19 +47,38 @@ public class SpeedTest : MonoBehaviour
             Debug.Log("up");
         }
         if (isDown) {
-            target.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (!flipHand)
+            {
+                target_left.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            else {
+                target_right.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            
         }
         if (isDown && notCross)
         {
             float delta = Vector2.Distance(lastMousePos, new Vector2(Input.mousePosition.x, Input.mousePosition.y)) / Mathf.Sqrt(Screen.width * Screen.width + Screen.height * Screen.height);
             float speed = delta / Time.deltaTime;
-            text.GetComponent<Text>().text = speed.ToString();
             lastMousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-            if (Input.mousePosition.x >= Screen.width / 2) {
-                notCross = false;
-                finalSpeed = speed;
-                hp.GetComponent<Slider>().value -= finalSpeed;
+            if (!flipHand)
+            {
+                if (Input.mousePosition.x >= Screen.width / 2)
+                {
+                    notCross = false;
+                    finalSpeed = speed;
+                    hp.GetComponent<Slider>().value -= finalSpeed;
+                }
             }
+            else {
+                if (Input.mousePosition.x <= Screen.width / 2)
+                {
+                    notCross = false;
+                    finalSpeed = speed;
+                    hp.GetComponent<Slider>().value -= finalSpeed;
+                }
+            }
+            
         }
     }
 
